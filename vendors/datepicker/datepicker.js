cf69335c9ce7087c9ca6658a51b66566ffb34659
muthangashem <muthangashem@gmail.com>
2019-03-05 22:17:07 +0300

    add modal content

--- vendors/datepicker/datepicker.js
+++ vendors/datepicker/datepicker.js
@@ -1,44 +1,9 @@
-<form>
-        <div class="form-row">
-            <div class="form-group col-md-4">
-                <label for="name">Name</label>
-                <input type="name" class="form-control" id="name" placeholder="Your name...">
-            </div>
-            <div class="form-group col-md-4">
-                <label for="email">Email</label>
-                <input type="email" class="form-control" id="email" placeholder="Email">
-            </div>
-            <div class="form-group col-md-4">
-                <label for="number">number</label>
-                <input type="number" class="form-control" id="number" placeholder="Phone Number">
-            </div>
-        </div>
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="inputState">Service</label>
-                <select id="inputState" class="form-control">
-                <option selected>Admission Essays</option>
-                <option>Annotated Bibliographies</option>
-                <option>MA/MSC/MBA/MPM Proposal/Project</option>
-                <option>PhD Thesis Development</option>
-                <option>Data Collection And Analysis</option>
-                <option>Journal Article Development</option>
-                <option>Publishing</option>
-                <option>Report Writing and Review</option>
-                <option>Research Support</option>
-                </select>
-            </div>
-            <div class="form-group col-md-6">
-                <div class="dates">
-                    <label for="">Deadline</label>
-                    <input type="text" autocomplete="off" class="form-control" id="user1">
-                </div>
-            </div>
-        </div>
-
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Let's Work Together</button>
-        </div>
-
-    </form>
+var $j = jQuery.noConflict();
+$('#example .input-group.date').datepicker({
+    weekStart: 1,
+    startDate: "today",
+    clearBtn: true,
+    autoclose: true,
+    todayHighlight: true,
+    toggleActive: true
+});
